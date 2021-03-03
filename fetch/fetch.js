f1a6ac12bdba97326e66fc6ffa70fd222e294eed
Adrian <57416882+aadrianras@users.noreply.github.com>
2021-03-03 09:54:51 -0400

    Update fetch.js

--- fetch/fetch.js
+++ fetch/fetch.js
@@ -24,15 +24,29 @@ export const fetchConToken = async (endpoint, data, method = 'GET') => {
     const url = `${baseURL}/${endpoint}`;
     const token = localStorage.getItem('token') || '';
 
-    return fetch(url, {
-          method,
-          body: JSON.stringify(data),
-          mode: 'cors',
-          credentials: 'same-origin',
-          headers: {
-              'Content-Type': 'application/json',
-              'x-token': token
-          },
-      }
-    );
+    if (method === 'GET') {
+        return fetch(url, {
+                method,
+                mode: 'cors',
+                credentials: 'same-origin',
+                headers: {
+                    'x-token': token
+                },
+            }
+        );
+        
+    } else {
+
+        return fetch(url, {
+                method,
+                body: JSON.stringify(data),
+                mode: 'cors',
+                credentials: 'same-origin',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'x-token': token
+                },
+            }
+        );
+    };
 };
